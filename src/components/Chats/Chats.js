ca9b99460cba6a6106f02c92d7077e7ffbefcbf8
Steven <steven.camillieri@protonmail.com>
2022-10-27 12:23:29 -0400

    working on markAllAsRead function

--- src/components/Chats/Chats.js
+++ src/components/Chats/Chats.js
@@ -2,39 +2,77 @@ import React, { useState } from "react";
 import { imagesJSON } from "../../resources/images/_imagesJSON";
 import "./Chats.css";
 
-export function Chats() {
+export function Chats({ handleClick }) {
   const [isNewMessage] = useState(true);
 
-  const profilePics = imagesJSON
-    // .filter((i) => i.id === 1)
-    .map((x) => (
-      <div
-        className={
-          isNewMessage ? "chatHeadContainer unreadChat" : "chatHeadContainer"
-        }
-      >
-        <div>
-          <img src={x.img} alt="x.alt" className="profilePic" />
-        </div>
-        <div>
-          {x.joined && (
-            <p>
-              <span className="boldText">{x.name}</span> has joined your group{" "}
-              <span className="boldText blueText">Chess Club</span>
-            </p>
-          )}
-          {x.react && (
-            <p>
-              <span className="boldText">{x.name}</span> reacted to your recent
-              post!
-            </p>
-          )}
-          {x.msg && <p>{x.msg}</p>}
-          {isNewMessage && <div className="newMessage"></div>}
-          <p className="chatHeadTime">1m ago</p>
-        </div>
+  const profilePics = imagesJSON.map((x) => (
+    <div
+      className={
+        isNewMessage ? "chatHeadContainer unreadChat" : "chatHeadContainer"
+      }
+      onClick={handleClick}
+    >
+      <div>
+        <img src={x.img} alt="x.alt" className="profilePic" />
       </div>
-    ));
+      <div>
+        
+        
+        
+        
+        <p hidden>
+          {x.key}
+          {x.isRead}
+          </p>
+        
+
+
+
+        {x.joined && (
+          <p>
+            <span className="boldText">{x.name}</span> has joined your group{" "}
+            <span className="boldText blueText">Chess Club</span>{" "}
+            {isNewMessage && <span className="newMessage"></span>}
+          </p>
+        )}
+        {x.left && (
+          <p>
+            <span className="boldText">{x.name}</span> left the group{" "}
+            <span className="boldText blueText">Chess Club</span>{" "}
+            {isNewMessage && <span className="newMessage"></span>}
+          </p>
+        )}
+        {x.react && (
+          <p>
+            <span className="boldText">{x.name}</span> reacted to your recent
+            post! {isNewMessage && <span className="newMessage"></span>}
+          </p>
+        )}
+        {x.follow && (
+          <p>
+            <span className="boldText">{x.name}</span> followed you{" "}
+            {isNewMessage && <span className="newMessage"></span>}
+          </p>
+        )}
+        {x.comment && (
+          <p>
+            <span className="boldText">{x.name}</span> commented on your picture{" "}
+            {isNewMessage && <span className="newMessage"></span>}
+          </p>
+        )}
+        {x.msg && (
+          <p>
+            <span className="boldText">{x.name}</span> sent you a private
+            meessage
+            {isNewMessage && <span className="newMessage"></span>}
+          </p>
+        )}
+
+        <p className="chatHeadTime">1m ago</p>
+        {x.msg && <div className="msgText">{x.msg}</div>}
+      </div>
+    </div>
+  ));
 
   return <main className="chatsMain">{profilePics}</main>;
 }
--- src/pages/NotificationPage/NotificationPage.js
+++ src/pages/NotificationPage/NotificationPage.js
@@ -1,33 +1,50 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
 import "./NotificationPage.css";
 import { Chats } from "../../components/Chats/Chats";
+import { imagesJSON } from "../../resources/images/_imagesJSON";
 
 export function Notifications() {
-  
-  
-  const [isRead, setIsRead] = useState({})
-  
-  function readTheMsg () {
-    setIsRead({
-      ...isRead,
-      
+  const [isRead, setIsRead] = useState(
+    imagesJSON.map((i, index) => {
+      return { index: i.key, isRead: i.isRead };
     })
+  );
+
+  function readTheMsg(e) {
+    // const value = e.target.value;
+
+    console.log(e);
+    console.log("clicked");
+    // setIsRead({
+    //   ...isRead,
+    //   [key]: !value,
+    // });
   }
 
+  function markAllAsRead() {
+    setIsRead(
+      imagesJSON.map((i, index) => {
+        return { index: i.key, isRead: true };
+      })
+    );
+  }
+
+  function checkIfRead(x) {
+    return x.isRead === false;
+  }
 
   return (
     <main className="notificationPageContainer">
-      
       <section className="headerSection">
         <div className="unreadTexts">
           <h3>Notifications</h3>
-          <div>3</div>
+          {isRead.some(checkIfRead) && <div>{isRead.length}</div>}
         </div>
-        <p >Mark all as read</p>
+        <p onClick={markAllAsRead}>Mark all as read</p>
       </section>
 
       <section className="chatsSection">
-        <Chats />
+        <Chats handleClick={(e) => readTheMsg(e)} />
       </section>
     </main>
   );
